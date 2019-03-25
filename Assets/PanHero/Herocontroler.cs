a61ca0ab80addb1291fabe4edd752b2e7600ced4
Edeter <jakubeklech@o2.pl>
2019-03-25 10:02:04 +0100

    init v2

---
+++ Assets/PanHero/Herocontroler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Herocontroler : MonoBehaviour
+{
+
+    Quaternion direction;
+    Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Flip(body);
+    }
+
+    void Flip(Rigidbody rb)
+    {
+        rb.MoveRotation(direction);
+    }
+}
