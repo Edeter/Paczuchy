a811085afd9bc315f36b22df0ce68efd46a2e7bf
bazillos <bazillos340@gmail.com>
2019-04-06 01:55:12 +0200

    npc base
    - basic npc mesh
    - camera follow with adjustable distance and angle

---
+++ Assets/BB_assets/hero_prefabs/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject target;
+    public float radius;
+    public float fi;
+    private float last_r;
+    private float last_deg;
+
+	// Use this for initialization
+	void Start () {
+        ///init
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y + radius*Mathf.Cos(-Mathf.Deg2Rad*fi), target.transform.position.z + radius*Mathf.Sin(-Mathf.Deg2Rad*fi));
+        transform.LookAt(target.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(last_r != radius || last_deg != fi)
+        {
+            transform.LookAt(target.transform.position);
+            last_r = radius;
+            last_deg = fi;
+        }
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y + radius * Mathf.Cos(-Mathf.Deg2Rad*fi), target.transform.position.z + radius * Mathf.Sin(-fi*Mathf.Deg2Rad));
+    }
+}
