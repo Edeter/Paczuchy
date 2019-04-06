99b3b3d3e53c3edee5ff5df6e444545acee048e6
bazillos <bazillos340@gmail.com>
2019-04-07 01:16:18 +0200

    Smooth camera follow v1.0
    but also jittery

--- Assets/BB_assets/hero_prefabs/CameraFollow.cs
+++ Assets/BB_assets/hero_prefabs/CameraFollow.cs
@@ -8,6 +8,9 @@ public class CameraFollow : MonoBehaviour {
     public float fi;
     private float last_r;
     private float last_deg;
+    [Range(0.02f, 5f)]
+    public float smoothFactor = 0.5f;
+    Vector3 outVelocity = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
@@ -17,13 +20,18 @@ public class CameraFollow : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+
+    //late update to make sure that player position was updated before camera
+	void LateUpdate () {
         if(last_r != radius || last_deg != fi)
         {
             transform.LookAt(target.transform.position);
             last_r = radius;
             last_deg = fi;
         }
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y + radius * Mathf.Cos(-Mathf.Deg2Rad*fi), target.transform.position.z + radius * Mathf.Sin(-fi*Mathf.Deg2Rad));
+        Vector3 newPosition = new Vector3(target.transform.position.x, target.transform.position.y + radius * Mathf.Cos(-Mathf.Deg2Rad*fi), target.transform.position.z + radius * Mathf.Sin(-fi*Mathf.Deg2Rad));
+        Vector3 posDiff = newPosition - transform.position;
+        //transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref outVelocity, smoothFactor);
+        transform.position += posDiff * smoothFactor * Time.deltaTime;
     }
 }
