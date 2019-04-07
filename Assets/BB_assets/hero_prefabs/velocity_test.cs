767d1676f00964e53047e3ed9b4b61c89a6bd8b4
bazillos <bazillos340@gmail.com>
2019-04-07 21:40:04 +0200

    camera follow v1.0 testing
    - velocity_test for testing object's velocity

---
+++ Assets/BB_assets/hero_prefabs/velocity_test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class velocity_test : MonoBehaviour {
+    public Rigidbody rb;
+    public Text text;
+    public float dt = 0.1f;
+    float lasttime;
+	// Use this for initialization
+	void Start () {
+        lasttime = Time.time;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(rb.velocity.magnitude);
+        if(lasttime + dt < Time.time)
+        {
+            lasttime = Time.time;
+            text.text = "velocity: " + rb.velocity.magnitude.ToString();
+        }
+	}
+}
