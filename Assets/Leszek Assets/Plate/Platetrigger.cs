353ba4cac89b2c72ae4b16b264be3f6acfd0cc71
Edeter <jakubeklech@o2.pl>
2019-04-09 22:57:53 +0200

    stash plate basics basicly nothing

---
+++ Assets/Leszek Assets/Plate/Platetrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platetrigger : MonoBehaviour {
+
+	[SerializeField] Collider[] collist;
+	public LayerMask m_LayerMask;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mycollision();
+	}
+	void mycollision()
+	{
+		collist = Physics.OverlapSphere(gameObject.transform.position, gameObject.GetComponent<SphereCollider>().radius, m_LayerMask);
+	}
+
+}
